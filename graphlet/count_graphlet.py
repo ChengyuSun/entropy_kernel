d22d3756abbd3552ac393bcee47f6c3d6b97f72b
scy <1294974924@qq.com>
2020-09-18 14:11:20 +0800

    09181411

--- graphlet/count_graphlet.py
+++ graphlet/count_graphlet.py
@@ -1,3 +1,15 @@
-def count_label_graphlet_2(begin_index,Adj):
+import copy
+
+
+def count_label_graphlet_chain(begin_index,chain_lenth,Adj,N):
+    a=copy.copy(Adj)
+    for i in range(N):
+        stack=[]
+        stack.append(a)
+        if find_chain()
+    return
+
+
+def generate_graphlet_index(label_num):
 
     return
